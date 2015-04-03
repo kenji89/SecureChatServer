e78a7347f43a853f62beeceb4b9aa64f1da27696
Marcin Marczyk <marcin.marczyk@capgemini.com>
2015-04-03 13:56:03 +0200

    added core

--- src/main/java/pl/marczykm/SecureChatServer/App.java
+++ src/main/java/pl/marczykm/SecureChatServer/App.java
@@ -1,13 +1,55 @@
 package pl.marczykm.SecureChatServer;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App extends Thread {
+	private ServerSocket serverSocket;
+
+	public App(int port) throws IOException {
+		serverSocket = new ServerSocket(port);
+		serverSocket.setSoTimeout(10000);
+	}
+
+	public void run() {
+		while (true) {
+			try {
+				System.out.println("Waiting for client on port "
+						+ serverSocket.getLocalPort() + "...");
+				Socket server = serverSocket.accept();
+				System.out.println("Just connected to "
+						+ server.getRemoteSocketAddress());
+				DataInputStream in = new DataInputStream(
+						server.getInputStream());
+				System.out.println(in.readUTF());
+				DataOutputStream out = new DataOutputStream(
+						server.getOutputStream());
+				out.writeUTF("Thank you for connecting to "
+						+ server.getLocalSocketAddress() + "\nGoodbye!");
+				server.close();
+			} catch (SocketTimeoutException e) {
+				System.err.println("Socket timed out!");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int port = Integer.parseInt(args[0]);
+		try {
+			Thread t = new App(port);
+			t.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
---
+++ start.bat
@@ -0,0 +1,1 @@
+java -jar target/SecureChatServer-0.0.1-SNAPSHOT-jar-with-dependencies.jar 1000
