c7169b8552ad12fd893e299923ba15bfe7a64abb
Marcin Marczyk <marcin.marczyk@capgemini.com>
2015-04-13 11:04:16 +0200

    added bad tests

--- src/main/java/pl/marczykm/SecureChatServer/entity/User.java
+++ src/main/java/pl/marczykm/SecureChatServer/entity/User.java
@@ -54,6 +54,48 @@ public class User {
 	public void setFriends(List<User> friends) {
 		this.friends = friends;
 	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((friends == null) ? 0 : friends.hashCode());
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		result = prime * result
+				+ ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (friends == null) {
+			if (other.friends != null)
+				return false;
+		} else if (!friends.equals(other.friends))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	
 	
 	
 }
